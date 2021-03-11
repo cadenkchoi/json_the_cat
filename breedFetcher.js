a9b3d9aa32c654d0bfcc6dad7cb39692a19d6772
Caden Choi <cadenc.7@gmail.com>
2021-03-11 22:47:37 +0000

    added a test assertions file using mocha chai, and index that calls the breedFetcher file to search api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,17 @@
 const request = require('request');
 
-// const fetchBreed = function (breedName, callback) {
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    callback(error, null);
-  }
-});
-// }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+    }
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback("Breed not found :(", null);
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
